f4b36a1ef5689b85a02cb56627985cf9f2e768d4
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-04-18 11:56:08 +0800

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Gerry Low on 2020-04-18.
+//  Copyright © 2020 Gerry Low. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
