9fbbe51a847aaf526345aa6bb821eeebc898a2a2
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-04-21 14:25:52 +0800

    Bug Fix - fix a keyboard adjustment bug

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -113,7 +113,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @objc func keyboardWillShow(_ notification: Notification) {
         if bottomTextField.isFirstResponder {
-            view.frame.origin.y -= getKeyboardHeight(notification)
+            view.frame.origin.y = -getKeyboardHeight(notification)
         }
     }
     
